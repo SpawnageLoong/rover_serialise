1c92bc3277530f1c8da35f990cd0f4251bad4d58
SpawnageLoong <richardloongcj@gmail.com>
2023-09-05 18:07:03 +0800

    Update code for successful build

--- src/rover_serialise.cpp
+++ src/rover_serialise.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/uint8.msg"
+#include "std_msgs/msg/u_int8.hpp"
 
 class MotorSerialiser : public rclcpp::Node
 {
@@ -7,41 +7,26 @@ class MotorSerialiser : public rclcpp::Node
         MotorSerialiser()
         : Node("motor_serialiser")
         {
-            subscription0_ = this->create_subscription<std_msgs::msg::UInt8>(
-                "/motors_pwm/" + this.motor_num0, 10, std::bind(&MotorSerialiser::motor0_callback, this, _1)
-            );
-            subscription1_ = this->create_subscription<std_msgs::msg::UInt8>(
-                "/motors_pwm/" + this.motor_num1, 10, std::bind(&MotorSerialiser::motor1_callback, this, _1)
-            );
-            subscription2_ = this->create_subscription<std_msgs::msg::UInt8>(
-                "/motors_pwm/" + this.motor_num2, 10, std::bind(&MotorSerialiser::motor2_callback, this, _1)
+            subscription_ = this->create_subscription<std_msgs::msg::UInt8>(
+                "/motors_pwm/" + motor_num0, 10, std::bind(&MotorSerialiser::motor0_callback, this, std::placeholders::_1)
             );
         }
 
     private:
-        char motor_num0, motor_num1, motor_num2;
-        uint8 pwm0, pwm1, pwm2;
+        char motor_num0;
+        char motor_num1;
+        char motor_num2;
         void motor0_callback(const std_msgs::msg::UInt8 &msg) const
         {
             // Callback code goes here
         }
-        void motor1_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            // Callback code goes here
-        }
-        void motor2_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            // Callback code goes here
-        }
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription0_;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription1_;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription2_;
+        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_;
 };
 
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MotorSerialiser>())
+    rclcpp::spin(std::make_shared<MotorSerialiser>());
     rclcpp::shutdown();
     return 0;
 }
