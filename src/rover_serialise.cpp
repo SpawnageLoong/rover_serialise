d9064ac64b77335d6129c468afe0f088577360f8
SpawnageLoong <richardloongcj@gmail.com>
2023-09-07 15:37:37 +0800

    Change topic_name strings to ros params

--- src/rover_serialise.cpp
+++ src/rover_serialise.cpp
@@ -17,42 +17,49 @@
 class MotorSerialiser : public rclcpp::Node
 {
     public:
-        MotorSerialiser(char *side)
+        MotorSerialiser()
         : Node("motor_serialiser")
         {
+            this->declare_parameter("topic0_name", "/motors_pwm/motor0");
+            this->declare_parameter("topic1_name", "/motors_pwm/motor1");
+            this->declare_parameter("topic2_name", "/motors_pwm/motor2");
+            this->declare_parameter("topic3_name", "/motors_pwm/motor3");
+            this->declare_parameter("topic4_name", "/motors_pwm/motor4");
+            this->declare_parameter("topic5_name", "/motors_pwm/motor5");
+
             subscription_0 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic0_name,
+                    this->get_parameter("topic0_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor0_callback, this, std::placeholders::_1)
                 );
             subscription_1 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic1_name,
+                    this->get_parameter("topic1_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor1_callback, this, std::placeholders::_1)
                 );
             subscription_2 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic2_name,
+                    this->get_parameter("topic2_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor2_callback, this, std::placeholders::_1)
                 );
             subscription_3 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic3_name,
+                    this->get_parameter("topic3_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor3_callback, this, std::placeholders::_1)
                 );
             subscription_4 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic4_name,
+                    this->get_parameter("topic4_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor4_callback, this, std::placeholders::_1)
                 );
             subscription_5 =
                 this->create_subscription<std_msgs::msg::UInt8>(
-                    topic5_name,
+                    this->get_parameter("topic5_name").as_string(),
                     rclcpp::SensorDataQoS(),
                     std::bind(&MotorSerialiser::motor5_callback, this, std::placeholders::_1)
                 );
@@ -64,12 +71,6 @@ class MotorSerialiser : public rclcpp::Node
         }
 
     private:
-        std::string topic0_name = "/motors_pwm/motor0";
-        std::string topic1_name = "/motors_pwm/motor1";
-        std::string topic2_name = "/motors_pwm/motor2";
-        std::string topic3_name = "/motors_pwm/motor3";
-        std::string topic4_name = "/motors_pwm/motor4";
-        std::string topic5_name = "/motors_pwm/motor5";
         uint8_t motor0_pwm = 0;
         uint8_t motor1_pwm = 0;
         uint8_t motor2_pwm = 0;
@@ -105,7 +106,7 @@ class MotorSerialiser : public rclcpp::Node
         {
             RCLCPP_INFO(this->get_logger(), "Timer callback");
             // TODO: Write the PWM values to the two byte arrays and send over serial
-            
+
         }
 
         rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_0;
@@ -120,7 +121,7 @@ class MotorSerialiser : public rclcpp::Node
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<MotorSerialiser>(argv[1]);
+    auto node = std::make_shared<MotorSerialiser>();
     std::cout << "MotorSerialiser started" << std::endl;
     rclcpp::spin(node);
     rclcpp::shutdown();
