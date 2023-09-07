35a8bfc74bb93609f1e251618703ed6bed834778
SpawnageLoong <richardloongcj@gmail.com>
2023-09-07 11:13:16 +0800

    Add minimum subscriber

--- src/rover_serialise.cpp
+++ src/rover_serialise.cpp
@@ -7,18 +7,19 @@ class MotorSerialiser : public rclcpp::Node
         MotorSerialiser()
         : Node("motor_serialiser")
         {
-            subscription_ = this->create_subscription<std_msgs::msg::UInt8>(
-                "/motors_pwm/" + motor_num0, 10, std::bind(&MotorSerialiser::motor0_callback, this, std::placeholders::_1)
-            );
+            subscription_ =
+                this->create_subscription<std_msgs::msg::UInt8>(
+                    "/motors_pwm/motor0",
+                    rclcpp::SensorDataQoS(),
+                    std::bind(&MotorSerialiser::motor0_callback, this, std::placeholders::_1)
+                );
         }
 
     private:
-        char motor_num0 = '0';
-        char motor_num1 = '1';
-        char motor_num2 = '2';
         void motor0_callback(const std_msgs::msg::UInt8 &msg) const
         {
             // Callback code goes here
+            RCLCPP_INFO(this->get_logger(), "I heard: '%u'", msg.data);
         }
         rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_;
 };
@@ -26,7 +27,12 @@ class MotorSerialiser : public rclcpp::Node
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MotorSerialiser>());
+    printf("RCLCPP Initialised\n");
+    auto node = std::make_shared<MotorSerialiser>();
+    printf("Node Created\n");
+    rclcpp::spin(node);
+    printf("Node Spun\n");
     rclcpp::shutdown();
+    printf("RCLCPP Shutdown\n");
     return 0;
 }
