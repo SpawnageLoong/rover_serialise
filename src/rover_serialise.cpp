38b09dd9d455c2ed5e1e9281b2f6cbbc6aed0ce8
SpawnageLoong <richardloongcj@gmail.com>
2023-09-19 15:33:09 +0800

    Add serial port support

--- src/rover_serialise.cpp
+++ src/rover_serialise.cpp
@@ -11,7 +11,10 @@
 
 #include <chrono>
 #include <iostream>
-#include <libusb.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int8.hpp"
 
@@ -118,6 +121,78 @@ class MotorSerialiser : public rclcpp::Node
         rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_5;
         rclcpp::TimerBase::SharedPtr timer;
 };
+
+class SerialPort {
+    public:
+        SerialPort(const char* portName, speed_t baudRate)
+        {
+            fd_ = open(portName, O_RDWR);
+            if (fd_ == -1)
+            {
+                std::cout << "Error opening serial port" << std::endl;
+                return;
+            }
+
+            struct termios tty;
+            if (tcgetattr(fd_, &tty) != 0)
+            {
+                std::cout << "Error getting serial port attributes" << std::endl;
+                return;
+            }
+
+            cfsetospeed(&tty, baudRate);
+            cfsetispeed(&tty, baudRate);
+
+            tty.c_cflag &= ~PARENB;
+            tty.c_cflag &= ~CSTOPB;
+            tty.c_cflag &= ~CSIZE;
+            tty.c_cflag |= CS8;
+            tty.c_cflag &= ~CREAD;
+            tty.c_cflag |= CLOCAL;
+
+            if (tcsetattr(fd_, TCSANOW, &tty) != 0)
+            {
+                std::cout << "Error setting serial port attributes" << std::endl;
+                return;
+            }
+        }
+
+        ~SerialPort()
+        {
+            if (isOpen())
+            {
+                close(fd_);
+            }
+        }
+
+        bool isOpen()
+        {
+            return fd_ != -1;
+        }
+
+        bool WriteData(const char* data, size_t length)
+        {
+            if (!isOpen())
+            {
+                return false;
+            }
+            ssize_t bytesWritten = write(fd_, data, length);
+            return bytesWritten == static_cast<ssize_t>(length);
+        }
+
+        size_t readData(char* buffer, size_t maxLength)
+        {
+            if (!isOpen())
+            {
+                return -1;
+            }
+            ssize_t bytesRead = read(fd_, buffer, maxLength);
+            return bytesRead;
+        }
+
+    private:
+        int fd_;
+}
 
 int main(int argc, char* argv[])
 {
