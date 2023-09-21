e6625f14e803e0ee38fcd6bdac79d3c0397ac216
Richard Loong <richardloongcj@gmail.com>
2023-09-21 11:29:20 +0800

    Change to PwmArray msg type

--- src/rover_serialise.cpp
+++ src/rover_serialise.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int8.hpp"
+#include "rover_interfaces/msg/pwm_array.hpp"
 
 class MotorSerialiser : public rclcpp::Node
 {
@@ -24,48 +25,13 @@ class MotorSerialiser : public rclcpp::Node
         MotorSerialiser()
         : Node("motor_serialiser")
         {
-            this->declare_parameter("topic0_name", "/motors_pwm/motor0");
-            this->declare_parameter("topic1_name", "/motors_pwm/motor1");
-            this->declare_parameter("topic2_name", "/motors_pwm/motor2");
-            this->declare_parameter("topic3_name", "/motors_pwm/motor3");
-            this->declare_parameter("topic4_name", "/motors_pwm/motor4");
-            this->declare_parameter("topic5_name", "/motors_pwm/motor5");
+            this->declare_parameter("pwm_array_topic_name", "/motors_pwm");
 
-            subscription_0 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic0_name").as_string(),
+            subscription_ =
+                this->create_subscription<rover_interfaces::msg::PwmArray>(
+                    this->get_parameter("pwm_array_topic_name").as_string(),
                     rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor0_callback, this, std::placeholders::_1)
-                );
-            subscription_1 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic1_name").as_string(),
-                    rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor1_callback, this, std::placeholders::_1)
-                );
-            subscription_2 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic2_name").as_string(),
-                    rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor2_callback, this, std::placeholders::_1)
-                );
-            subscription_3 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic3_name").as_string(),
-                    rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor3_callback, this, std::placeholders::_1)
-                );
-            subscription_4 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic4_name").as_string(),
-                    rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor4_callback, this, std::placeholders::_1)
-                );
-            subscription_5 =
-                this->create_subscription<std_msgs::msg::UInt8>(
-                    this->get_parameter("topic5_name").as_string(),
-                    rclcpp::SensorDataQoS(),
-                    std::bind(&MotorSerialiser::motor5_callback, this, std::placeholders::_1)
+                    std::bind(&MotorSerialiser::pwm_callback, this, std::placeholders::_1)
                 );
             timer =
                 this->create_wall_timer(
@@ -75,36 +41,16 @@ class MotorSerialiser : public rclcpp::Node
         }
 
     private:
-        uint8_t motor0_pwm = 0;
-        uint8_t motor1_pwm = 0;
-        uint8_t motor2_pwm = 0;
-        uint8_t motor3_pwm = 0;
-        uint8_t motor4_pwm = 0;
-        uint8_t motor5_pwm = 0;
+        int16_t motor0_pwm = 0;
+        int16_t motor1_pwm = 0;
+        int16_t motor2_pwm = 0;
+        int16_t motor3_pwm = 0;
+        int16_t motor4_pwm = 0;
+        int16_t motor5_pwm = 0;
 
-        void motor0_callback(const std_msgs::msg::UInt8 &msg) const
+        void pwm_callback(const rover_interfaces::msg::PwmArray &msg) const
         {
-            RCLCPP_INFO(this->get_logger(), "Motor 0 received: '%u'", msg.data);
-        }
-        void motor1_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            RCLCPP_INFO(this->get_logger(), "Motor 1 received: '%u'", msg.data);
-        }
-        void motor2_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            RCLCPP_INFO(this->get_logger(), "Motor 2 received: '%u'", msg.data);
-        }
-        void motor3_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            RCLCPP_INFO(this->get_logger(), "Motor 3 received: '%u'", msg.data);
-        }
-        void motor4_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            RCLCPP_INFO(this->get_logger(), "Motor 4 received: '%u'", msg.data);
-        }
-        void motor5_callback(const std_msgs::msg::UInt8 &msg) const
-        {
-            RCLCPP_INFO(this->get_logger(), "Motor 5 received: '%u'", msg.data);
+            RCLCPP_INFO(this->get_logger(), "Array PWM0 received: '%u'", msg.pwm0);
         }
         void timer_callback()
         {
@@ -113,12 +59,7 @@ class MotorSerialiser : public rclcpp::Node
 
         }
 
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_0;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_1;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_2;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_3;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_4;
-        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_5;
+        rclcpp::Subscription<rover_interfaces::msg::PwmArray>::SharedPtr subscription_;
         rclcpp::TimerBase::SharedPtr timer;
 };
 
